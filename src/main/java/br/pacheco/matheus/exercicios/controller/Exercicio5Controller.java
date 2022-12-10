280d1d8b58e40b4a4da13460d12ad4f2d959fd28
matheus1996p <matheus_pachecosilva@hotmail.com>
2022-12-10 14:01:33 -0300

    Exercicio 5 - DELETE

--- src/main/java/br/pacheco/matheus/exercicios/controller/Exercicio5Controller.java
+++ src/main/java/br/pacheco/matheus/exercicios/controller/Exercicio5Controller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -76,6 +77,20 @@ public class Exercicio5Controller {
 			return new ResponseEntity<>(
 					veiculoRepository.save(veiculo),
 					HttpStatus.CREATED);
+		} catch (Exception e) {
+			e.printStackTrace();
+            return null;
+		}
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity excluirVeiculo(@PathVariable Long id) {
+		try {
+			return veiculoRepository.findById(id)
+			           .map(veiculo -> {
+			        	   veiculoRepository.deleteById(id);
+			               return ResponseEntity.ok().build();
+			           }).orElse(ResponseEntity.notFound().build());
 		} catch (Exception e) {
 			e.printStackTrace();
             return null;
